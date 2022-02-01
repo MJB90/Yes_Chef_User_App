030a0fe1abd273dbee78ffa05ec0a84c19b217dd
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2022-02-01 23:42:16 +0800

    Still experimenting with the layout. Created a recipe class and ingredients class.

--- app/src/main/java/com/example/yeschefuserapp/MainActivity.java
+++ app/src/main/java/com/example/yeschefuserapp/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.yeschefuserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private final ArrayList<Recipe> recipes=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainCustomAdapter adapter=new MainCustomAdapter(this, recipes, recipe -> {
+            //Click on a recipe
+        });
+
+        RecyclerView recyclerView=findViewById(R.id.recycler_view);
+
+        if (recyclerView!=null)
+        {
+            LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(adapter);
+        }
+
     }
+
+
 }
