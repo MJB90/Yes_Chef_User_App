aea139e8e729b60e39e1c5c5bc122f13aca260ca
Yu Jing <e0703487@u.nus.edu>
2022-02-05 12:44:08 +0800

    Add recommendation and like view

--- app/src/main/java/com/example/yeschefuserapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/yeschefuserapp/fragment/HomeFragment.java
@@ -2,16 +2,15 @@ package com.example.yeschefuserapp.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
@@ -29,6 +28,8 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     private final List<Recipe> recipes = new ArrayList<>();
+    private final List<Recipe> recommendationRecipes = new ArrayList<>();
+    private final List<Recipe> likeRecipes = new ArrayList<>();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -46,22 +47,40 @@ public class HomeFragment extends Fragment {
             startActivity(intent);
         });
 
-        fetchData(adapter, view);
-        initView(adapter, view);
+        MainCustomAdapter recommendationAdapter = new MainCustomAdapter(view.getContext(), this.recommendationRecipes, recipe -> {
+            //Click on a recipe
+            Intent intent = new Intent(view.getContext(), ViewRecipeActivity.class);
+            intent.putExtra("recipeId", recipe.getId());
+            startActivity(intent);
+        });
+
+        MainCustomAdapter likeAdapter = new MainCustomAdapter(view.getContext(), this.likeRecipes, recipe -> {
+            //Click on a recipe
+            Intent intent = new Intent(view.getContext(), ViewRecipeActivity.class);
+            intent.putExtra("recipeId", recipe.getId());
+            startActivity(intent);
+        });
+
+        fetchData(adapter, view, "http://10.0.2.2:8090/api/user/all_recipes", this.recipes);
+        fetchData(recommendationAdapter, view, "http://10.0.2.2:8090/api/user/all_recipes", this.recommendationRecipes);
+        fetchData(likeAdapter, view, "http://10.0.2.2:8090/api/user/all_recipes", this.likeRecipes);
+        initView(adapter, view, view.findViewById(R.id.recycler_view));
+        initView(recommendationAdapter, view, view.findViewById(R.id.recommendation_view));
+        initView(likeAdapter, view, view.findViewById(R.id.like_view));
 
         return view;
     }
 
-    private void fetchData(MainCustomAdapter adapter, View view) {
+    private void fetchData(MainCustomAdapter adapter, View view, String url, List<Recipe> list) {
         JsonArrayRequest objectRequest = new JsonArrayRequest(
                 Request.Method.GET,
-                "http://10.0.2.2:8090/api/user/all_recipes",
+                url,
                 null,
                 response -> {
                     Gson gson = new Gson();
                     Recipe[] tmpArray = gson.fromJson(response.toString(), Recipe[].class);
                     // The recipes should be the same reference
-                    recipes.addAll(Arrays.asList(tmpArray.clone()));
+                    list.addAll(Arrays.asList(tmpArray.clone()));
 
                     // Need to notify the adapter after updating the recipes
                     // ref: https://stackoverflow.com/a/48959184
@@ -72,9 +91,7 @@ public class HomeFragment extends Fragment {
         MySingleton.getInstance(view.getContext()).addToRequestQueue(objectRequest);
     }
 
-    private void initView(MainCustomAdapter adapter, View view) {
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
-
+    private void initView(MainCustomAdapter adapter, View view, RecyclerView recyclerView) {
         if (recyclerView != null) {
             LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
             recyclerView.setLayoutManager(layoutManager);
