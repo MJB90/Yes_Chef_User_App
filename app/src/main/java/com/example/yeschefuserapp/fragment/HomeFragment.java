ddeef5ef89d6167635ff7796e1cee3b1a6fb8176
Yu Jing <e0703487@u.nus.edu>
2022-02-08 21:40:02 +0800

    Auto format and remove redundant code

--- app/src/main/java/com/example/yeschefuserapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/yeschefuserapp/fragment/HomeFragment.java
@@ -1,39 +1,28 @@
 package com.example.yeschefuserapp.fragment;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.yeschefuserapp.R;
-import com.example.yeschefuserapp.activity.ViewRecipeActivity;
-import com.example.yeschefuserapp.adapter.MainHorizontalCustomAdapter;
 import com.example.yeschefuserapp.adapter.MainVerticalCustomListAdapter;
 import com.example.yeschefuserapp.model.Recipe;
 import com.example.yeschefuserapp.model.RecipeCategoryList;
-import com.example.yeschefuserapp.utility.MySingleton;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private MainVerticalCustomListAdapter mainVerticalCustomListAdapter;
-    private List<RecipeCategoryList> recipeCategoryLists=new ArrayList<>();
-    private final List<Recipe> recipes = new ArrayList<>();
+    private List<RecipeCategoryList> recipeCategoryLists = new ArrayList<>();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -50,14 +39,13 @@ public class HomeFragment extends Fragment {
         //creating a list of recipe category list
         createRecipeCategoryList();
 
-        mainVerticalCustomListAdapter=new MainVerticalCustomListAdapter(viewContext,recipeCategoryLists);
+        mainVerticalCustomListAdapter = new MainVerticalCustomListAdapter(viewContext, recipeCategoryLists);
 
         initView(mainVerticalCustomListAdapter, view, view.findViewById(R.id.recycler_view));
 
 
         return view;
     }
-
 
 
     private void initView(MainVerticalCustomListAdapter adapter, View view, RecyclerView recyclerView) {
@@ -69,9 +57,9 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    private void createRecipeCategoryList(){
-        recipeCategoryLists.add(new RecipeCategoryList("Malaysian Food",new ArrayList<Recipe>(),"http://10.0.2.2:8090/api/user/all_recipes"));
-        recipeCategoryLists.add(new RecipeCategoryList("Singapore Food",new ArrayList<Recipe>(),"http://10.0.2.2:8090/api/user/all_recipes"));
-        recipeCategoryLists.add(new RecipeCategoryList("Thailand Food",new ArrayList<Recipe>(),"http://10.0.2.2:8090/api/user/all_recipes"));
+    private void createRecipeCategoryList() {
+        recipeCategoryLists.add(new RecipeCategoryList("Malaysian Food", new ArrayList<Recipe>(), "http://10.0.2.2:8090/api/user/all_recipes"));
+        recipeCategoryLists.add(new RecipeCategoryList("Singapore Food", new ArrayList<Recipe>(), "http://10.0.2.2:8090/api/user/all_recipes"));
+        recipeCategoryLists.add(new RecipeCategoryList("Thailand Food", new ArrayList<Recipe>(), "http://10.0.2.2:8090/api/user/all_recipes"));
     }
 }
