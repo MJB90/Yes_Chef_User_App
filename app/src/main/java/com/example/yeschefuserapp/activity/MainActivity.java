fae3e90598c51cd2cef399c3653d45fccc48dc98
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2022-02-10 00:01:37 +0800

    add location finder

--- app/src/main/java/com/example/yeschefuserapp/activity/MainActivity.java
+++ app/src/main/java/com/example/yeschefuserapp/activity/MainActivity.java
@@ -1,27 +1,95 @@
 package com.example.yeschefuserapp.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.example.yeschefuserapp.R;
 import com.example.yeschefuserapp.fragment.AccountFragment;
 import com.example.yeschefuserapp.fragment.AdvancedFilterFragment;
 import com.example.yeschefuserapp.fragment.BookmarksFragment;
 import com.example.yeschefuserapp.fragment.HomeFragment;
+import com.example.yeschefuserapp.model.Recipe;
+import com.example.yeschefuserapp.utility.MySingleton;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
+    private String country;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grantPermission();
+        checkLocationIsEnabledOrNot();
         navigationBar();
+    }
+
+    private void checkLocationIsEnabledOrNot() {
+        LocationManager lm=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnabled=false;
+        boolean networkEnabled=false;
+
+        try{
+            gpsEnabled=lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            networkEnabled=lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        if (!gpsEnabled && !networkEnabled){
+            new AlertDialog.Builder(this)
+                    .setTitle("Enable GPS Service")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    }).setNegativeButton("Cancel",null).show();
+        }
+    }
+
+    private void grantPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},44);
+        }
     }
 
     private void navigationBar(){
--- app/src/main/java/com/example/yeschefuserapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/yeschefuserapp/fragment/HomeFragment.java
@@ -2,12 +2,16 @@ package com.example.yeschefuserapp.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -15,7 +19,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.yeschefuserapp.R;
 import com.example.yeschefuserapp.activity.FilterResult;
@@ -23,19 +26,27 @@ import com.example.yeschefuserapp.adapter.MainVerticalCustomListAdapter;
 import com.example.yeschefuserapp.model.Recipe;
 import com.example.yeschefuserapp.model.RecipeCategoryList;
 import com.example.yeschefuserapp.utility.MySingleton;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
     private MainVerticalCustomListAdapter mainVerticalCustomListAdapter;
     private List<RecipeCategoryList> recipeCategoryLists = new ArrayList<>();
+    private String country;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -50,7 +61,7 @@ public class HomeFragment extends Fragment {
         Context viewContext = view.getContext();
 
         //creating a list of recipe category list
-        createRecipeCategoryList();
+        createRecipeCategoryList(viewContext);
 
         mainVerticalCustomListAdapter=new MainVerticalCustomListAdapter(viewContext,recipeCategoryLists);
         initView(mainVerticalCustomListAdapter, view, view.findViewById(R.id.recycler_view));
@@ -66,7 +77,7 @@ public class HomeFragment extends Fragment {
                     List<Recipe> recipeSearch = new ArrayList<>();
                     JsonArrayRequest objectRequest = new JsonArrayRequest(
                             Request.Method.GET,
-                            "http://10.0.2.2:8090/api/user/advanced_filter/search/"+s,
+                            "http://10.0.2.2:8090/api/user/search/"+s,
                             null,
                             response -> {
                                 Recipe[] tmpArray = gson.fromJson(response.toString(), Recipe[].class);
@@ -83,7 +94,6 @@ public class HomeFragment extends Fragment {
 
                 @Override
                 public boolean onQueryTextChange(String s) {
-                    Toast.makeText(view.getContext(),s,Toast.LENGTH_SHORT).show();
                     return true;
                 }
             });
@@ -102,9 +112,45 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    private void createRecipeCategoryList() {
+    private void createRecipeCategoryList(Context context) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");//To get the current time
+        LocalTime now = LocalTime.now();
+        String time=dtf.format(now).toString();
+
+
+
+        try{
+            FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>(){
+                @Override
+                public void onComplete(@NonNull Task<Location> task){
+                    Location location=task.getResult();
+                    if (location!=null){
+
+                        Geocoder geocoder=new Geocoder(context.getApplicationContext(), Locale.getDefault());
+                        List<Address> address= null;
+                        try {
+                            address = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+                            country = address.get(0).getCountryName();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }
+            });
+        }
+        catch(SecurityException e){
+            e.printStackTrace();
+        }
+
         recipeCategoryLists.add(new RecipeCategoryList("Malaysian Food", new ArrayList<Recipe>(), "http://10.0.2.2:8090/api/user/all_recipes"));
         recipeCategoryLists.add(new RecipeCategoryList("Singapore Food", new ArrayList<Recipe>(), "http://10.0.2.2:8090/api/user/all_recipes"));
         recipeCategoryLists.add(new RecipeCategoryList("Thailand Food", new ArrayList<Recipe>(), "http://10.0.2.2:8090/api/user/all_recipes"));
+    }
+
+    private void getLocation() {
+
     }
 }
