d0ac46004ad6561ea0a6a6904376b66f060d3fdc
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2022-02-16 17:07:29 +0800

    Implemented a function for the server to track the user view history.

--- app/src/main/java/com/example/yeschefuserapp/listener/RecipeClickListener.java
+++ app/src/main/java/com/example/yeschefuserapp/listener/RecipeClickListener.java
@@ -2,21 +2,63 @@ package com.example.yeschefuserapp.listener;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.yeschefuserapp.R;
+import com.example.yeschefuserapp.activity.LoginActivity;
 import com.example.yeschefuserapp.activity.ViewRecipeActivity;
+import com.example.yeschefuserapp.context.UserContext;
 import com.example.yeschefuserapp.model.Recipe;
+import com.example.yeschefuserapp.utility.MySingleton;
+import com.example.yeschefuserapp.utility.RecommendedRecipes;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RecipeClickListener implements ItemClickListener {
     private Context context;
+    private UserContext userContext;
+    private String ACCESS_TOKEN;
 
     public RecipeClickListener(Context context) {
         this.context = context;
+        userContext = new UserContext(context);
+        ACCESS_TOKEN= userContext.getToken();
     }
 
     @Override
     public void onItemClick(Recipe recipe) {
-        Intent intent = new Intent(context, ViewRecipeActivity.class);
-        intent.putExtra("recipe", recipe);
-        context.startActivity(intent);
+        //Saving the user view history
+        StringRequest objectRequest = new StringRequest(
+                Request.Method.POST,
+                String.format(context.getString(R.string.domain_name) + "api/user/post_user_activity/%s", recipe.getId()),
+                response -> {
+                    Intent intent = new Intent(context, ViewRecipeActivity.class);
+                    intent.putExtra("recipe", recipe);
+                    context.startActivity(intent);
+                },
+                error -> {
+                    Toast.makeText(context, "You are Logged out", Toast.LENGTH_LONG).show();
+                    userContext.clearLoginPreferences();
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headerMap = new HashMap<String, String>();
+                headerMap.put("Content-Type", "application/json");
+                headerMap.put("Authorization", "Bearer " + ACCESS_TOKEN);
+                return headerMap;
+            }
+        };
+        objectRequest.setRetryPolicy(new DefaultRetryPolicy(0, 0, DefaultRetryPolicy.DEFAULT_TIMEOUT_MS));
+        MySingleton.getInstance(context).addToRequestQueue(objectRequest);
     }
 }
